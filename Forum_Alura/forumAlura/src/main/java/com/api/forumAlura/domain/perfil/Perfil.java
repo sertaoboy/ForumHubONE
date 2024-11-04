30945817dce6dd35aa3fe1898929383ad003932c
Raul H <raul.dev@hotmail.com>
2024-11-04 11:30:51 -0300

    editing constructor for receive DTO UsuarioCadastro

--- Forum_Alura/forumAlura/src/main/java/com/api/forumAlura/domain/perfil/Perfil.java
+++ Forum_Alura/forumAlura/src/main/java/com/api/forumAlura/domain/perfil/Perfil.java
@@ -1,5 +1,8 @@
 package com.api.forumAlura.domain.perfil;
 
+import com.api.forumAlura.domain.usuario.DadosCadastroUsuario;
+import com.api.forumAlura.domain.usuario.Usuario;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import lombok.*;
@@ -20,16 +23,26 @@ public class Perfil {
     private Long id;
 
     @NotBlank
+    @Column(unique = true)
     private String nome;
 
 
     private Boolean ativo;
 
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "usuario_id", nullable = false)
+    @JsonIgnore //ignora usuario ao serializar o perfil
+    private Usuario usuario;
 
+    public Perfil (DadosCadastroUsuario dadosUsuarioCadastro){
+        this.ativo = true;
+        this.nome= dadosUsuarioCadastro.nome();
+
+    }
 
     public Perfil(DadosCadastroPerfil dados) {
-        this.nome= dados.nome();
         this.ativo = true;
+        this.nome= dados.nome();
     }
 
     public void atualizarInformacoes(DadosAtualizacaoPerfil dados) {
