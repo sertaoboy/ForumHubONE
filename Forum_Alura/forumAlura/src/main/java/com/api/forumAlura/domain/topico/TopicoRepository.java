7d502b00a55725ec036b0c4f2f5d6a939d23736e
Raul H <raul.dev@hotmail.com>
2024-11-06 11:58:30 -0300

    adding repository querie for topico.nome

--- Forum_Alura/forumAlura/src/main/java/com/api/forumAlura/domain/topico/TopicoRepository.java
+++ Forum_Alura/forumAlura/src/main/java/com/api/forumAlura/domain/topico/TopicoRepository.java
@@ -2,7 +2,9 @@ package com.api.forumAlura.domain.topico;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
 
 
 public interface TopicoRepository extends JpaRepository<Topico,Long> {
+    Optional<Topico> findByAutor_Nome(String nome);
 }
