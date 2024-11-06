0f7908c0680cb75f1e4530541ca636a36d5d4f0e
Raul H <raul.dev@hotmail.com>
2024-11-06 12:01:27 -0300

    adding new queries for find by nome and email

--- Forum_Alura/forumAlura/src/main/java/com/api/forumAlura/domain/usuario/UsuarioRepository.java
+++ Forum_Alura/forumAlura/src/main/java/com/api/forumAlura/domain/usuario/UsuarioRepository.java
@@ -3,9 +3,14 @@ package com.api.forumAlura.domain.usuario;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Optional;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
     Page<Usuario> findAllByAtivoTrue(Pageable paginacao);
+
+    UserDetails findByNome(String username);
+
+    Optional<Usuario> findByEmail(String email);
 }
