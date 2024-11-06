62b4216cc75b9ae325ca527b4b8f02c9353c1ef5
Raul H <raul.dev@hotmail.com>
2024-11-06 11:59:30 -0300

    adding overrided method from UserDetails

--- Forum_Alura/forumAlura/src/main/java/com/api/forumAlura/domain/usuario/AutenticacaoService.java
+++ Forum_Alura/forumAlura/src/main/java/com/api/forumAlura/domain/usuario/AutenticacaoService.java
@@ -1,6 +1,5 @@
-package com.api.forumAlura.domain.usuario.login;
+package com.api.forumAlura.domain.usuario;
 
-import com.api.forumAlura.domain.usuario.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -14,6 +13,6 @@ public class AutenticacaoService implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return repository.findByLogin(username);
+        return repository.findByNome(username);
     }
 }
