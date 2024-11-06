428a18d63a666ab56afb8f1be152212cf8240c44
Raul H <raul.dev@hotmail.com>
2024-11-06 11:55:58 -0300

    authentication feature on controller

--- Forum_Alura/forumAlura/src/main/java/com/api/forumAlura/controllers/AutenticacaoController.java
+++ Forum_Alura/forumAlura/src/main/java/com/api/forumAlura/controllers/AutenticacaoController.java
@@ -1,4 +1,34 @@
 package com.api.forumAlura.controllers;
 
+import com.api.forumAlura.domain.usuario.DadosAutenticacao;
+import com.api.forumAlura.domain.usuario.Usuario;
+import com.api.forumAlura.infra.security.DadosTokenJWT;
+import com.api.forumAlura.infra.security.TokenService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/login")
 public class AutenticacaoController {
+
+    @Autowired
+    private AuthenticationManager manager;
+
+    @Autowired
+    private TokenService tokenService;
+
+    @PostMapping
+    public ResponseEntity efetuarLogin(@RequestBody @Valid DadosAutenticacao dados){
+        var authenticationToken = new UsernamePasswordAuthenticationToken(dados.nome(),dados.senha());
+        var authentication = manager.authenticate(authenticationToken);
+        var tokenJWT = tokenService.gerarToken((Usuario) authentication.getPrincipal());
+        return ResponseEntity.ok(new DadosTokenJWT(tokenJWT));
+    }
 }
