f981ee6e1464ed3661cf6be6bf63a12f9867942a
Raul H <raul.dev@hotmail.com>
2024-11-07 12:50:46 -0300

    adding JPQL querie for equal topics

--- Forum_Alura/forumAlura/src/main/java/com/api/forumAlura/domain/topico/TopicoRepository.java
+++ Forum_Alura/forumAlura/src/main/java/com/api/forumAlura/domain/topico/TopicoRepository.java
@@ -1,5 +1,6 @@
 package com.api.forumAlura.domain.topico;
 
+import jakarta.validation.constraints.NotBlank;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
@@ -7,4 +8,7 @@ import java.util.Optional;
 
 public interface TopicoRepository extends JpaRepository<Topico,Long> {
     Optional<Topico> findByAutor_Nome(String nome);
+
+    boolean existsByTituloAndMensagem(@NotBlank String titulo, @NotBlank String mensagem);
+
 }
